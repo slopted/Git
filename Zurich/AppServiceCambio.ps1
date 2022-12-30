0702c255e29041a6fbf0427158cc0dd6e8cc5009
Slopted <slopted@gmail.com>
2022-12-30 09:08:47 -0300

    Zurich AppService Plan Scale

---
+++ Zurich/AppServiceCambio.ps1
@@ -0,0 +1,28 @@
+﻿####-----> Colocar RG correcto 
+$ResourceGroupName = "rg_zurich_lab"
+
+####-----> Colocar AppServicePlan correcto 
+$AppServicePlan = "ASP-rgzurichlab-b9e3"
+
+####-----> Colocar el Tier requerido para escalamiento vertical
+$Tier = "Free" 
+
+####-----> Colocar el Size requerido para escalamiento vertical
+$Size = "Small"
+
+####-----> Cambio de plan de escalamiento vertical
+Set-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlan -Tier "$Tier" -WorkerSize "$Size"
+
+####-----> Apaga y Enciende Aplicaciones para replicar escalamiento
+$app=Get-AzureRmAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlan
+
+$apps=Get-AzureRmWebApp -AppServicePlan $app
+
+Foreach($app in $apps)
+{
+ $websiteName = $app.Name
+ Restart-AzureRmWebApp -ResourceGroupName $app.ResourceGroup -Name $websiteName
+}
+
+####-----> Cambiar número de instancias requeridas en "NumberofWorkers" para escalamiento horizontal
+Set-AzAppServicePlan -NumberofWorkers 2 -Name $AppServicePlan -ResourceGroupName $ResourceGroupName
---
+++ Zurich/ScaleUp/LA-CL-APIPLAN-P3-1-Min.ps1
@@ -0,0 +1,17 @@
+Set-AzContext -Subscription "251f8d88-2d55-4b29-bb93-c4feb93b3f8a"
+
+$ResourceGroupName = "LA-CL-IaaS-Automation"
+$AppServicePlan = "LA-CL-APIPLAN-P3-1"
+$Tier = "Basic" 
+$Size = "Small"
+$app=Get-AzureRmAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlan
+$apps=Get-AzureRmWebApp -AppServicePlan $app
+
+Set-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlan -Tier "$Tier" -WorkerSize "$Size"
+Set-AzAppServicePlan -NumberofWorkers 1 -Name $AppServicePlan -ResourceGroupName $ResourceGroupName
+
+Foreach($app in $apps)
+{
+ $websiteName = $app.Name
+ Restart-AzureRmWebApp -ResourceGroupName $app.ResourceGroup -Name $websiteName
+}
