806863b53e1b0daa96a4e94ace39c67bb58c94c1
Slopted <slopted@gmail.com>
2024-02-16 06:12:20 -0300

    Layout Windows 11

--- BancoEstado/CertificateScript.ps1
+++ BancoEstado/CertificateScript.ps1
@@ -1,16 +1,18 @@
-$forest = "banco"   
+$forest = "banco.bestado.cl"   
 $file = "C:\temp\certs.csv"   
 $cred = Get-Credential ""   
 $out=(Get-ADForest -Identity  $forest).Domains | 
 ForEach-Object 
-            {       (Get-ADcomputer -Filter * -SearchBase "" -server $_).DNSHostName | ForEach-Object 
+            {       (Get-ADcomputer -Filter * -SearchBase "OU=IT, DC=contoso, DC=com" -server $_).DNSHostName | ForEach-Object 
                 {           Invoke-Command -ComputerName $_ -credential $cred -HideComputerName -ScriptBlock 
                     {                Get-ChildItem -Path Cert:\LocalMachine -Recurse | Where-Object {$_.PSISContainer -eq $false} |
                                      Select-Object -Property Thumbprint,Subject,Issuer,NotBefore,NotAfter
                     }
                 }
             } |
-Select-Object -Property * -ExcludeProperty RunSpaceID,PSShowComputerName | Export-Csv -Path $file -NoTypeInformation
+Select-Object -Property * -ExcludeProperty RunSpaceID,PSShowComputerName |
+Test-Path -Path "C:\temp\" |
+Export-Csv -Path $file -NoTypeInformation
 
 
 #Get-ADComputer -Filter * -SearchBase "OU=IT, DC=contoso, DC=com"
