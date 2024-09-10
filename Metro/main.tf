d4f95468a12e2399794e61b427bc0b97a5d5dd76
Slopted <slopted@gmail.com>
2024-09-10 11:02:25 -0300

    add terraform creation files

--- Metro/main.tf
+++ Metro/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg-POCMetroProd-001" {
 
 resource "azurerm_virtual_network" "vnet-POCMetroProd-001" {
   name                = local.vnet_name
-  location            = azurerm_resource_group.IaCTranzact-RG.location
-  resource_group_name = azurerm_resource_group.IaCTranzact-RG.name
+  location            = azurerm_resource_group.rg-POCMetroProd-001.location
+  resource_group_name = azurerm_resource_group.rg-POCMetroProd-001.name
   address_space       = ["10.0.0.0/24"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
